1929bd3c993c9a65d450538dcab19ddeb16ea865
Doug Hawkinson <doug5solas@gmail.com>
2018-10-05 18:34:29 -0700

    Added client-side app

--- index.js
+++ index.js
@@ -33,9 +33,31 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-//  routing
+//  routing -- handled by the Express Router
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+//  IN PRODUCTION,
+//  others need to be handled by routes in the client/build path (specific files)
+//  still others get passed off the the client/build/index.html file (by the React Router)
+if ( process.env.NODE_ENV === 'production' ) {
+    //  This block of routing code will operate sequentially -- first the app.use then the app.get
+    //  that is how the correct serving up of files takes place
+    //  Remember the previous routes have been interrogated already
+
+    //  Express will serve up production assets like our file
+    //  main.js & main.css
+    app.use(express.static('client/build'));
+
+    //  Express will serve up the index.html file
+    //  if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+
+}
 
 //  express tells node to listen for activity on a specific port
 const PORT = process.env.PORT || 5000;
